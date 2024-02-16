6b9640bdf9e0f01b8795f6fa4ebd1054e6bf94cc
Jinglin Huang <125185864+fenglianwanmao@users.noreply.github.com>
2024-02-16 13:58:44 +0800

    Update nw.js

--- nw.js
+++ nw.js
@@ -4,20 +4,31 @@ const svc = new Service({
 	name: 'unblock-netease-cloud-music',
 	description: '点亮网易云音乐灰色歌曲',
 	script: './app.js', // 入口文件路径
-	// scriptOptions:'-p 52100:52101', // 可选参数示例: 自定义端口并开启HTTPS
-	// scriptOptions: '-o qq', // 可选参数
+	scriptOptions: '-o qq kuwo bilibili -p 7890:7891', // 可选参数
 	wait: '1', // 程序崩溃后重启时间间隔
 	grow: '0.25', // 重启等待时间成长值，第一次1秒，第二次1.25秒。。。
 	maxRestarts: '40', // 60秒内最大重启次数
 	env: [
-		// {
-		// 	name: 'ENABLE_FLAC',
-		// 	value 'true',
-		// },
-		// {
-		// 	name: 'QQ_COOKIE',
-		// 	value: 'uin=<your_uin>; qm_keyst=<your_qm_keyst>',
-		// },
+		 {
+		 	name: 'ENABLE_FLAC',
+		 	value: 'true',
+		 },
+		{
+		 	name: 'QQ_COOKIE',
+			value: 'uin=<2373919927>; qm_keyst=<Q_H_L_5iu7_gVDDDKRMenJx7vp1EfOulJz0fN08CnXZAVQqaI7ZsIb1pnwh7w>',
+		},
+		 {
+			 name: 'ENABLE_LOCAL_VIP',
+			 value: 'svip',
+
+		 },
+		{
+			name: 'BLOCK_ADS',
+                         value: 'true',
+		 },{
+			name: 'FOLLOW_SOURCE_ORDER',
+                         value: 'true',
+		 },
 	],
 });
 
